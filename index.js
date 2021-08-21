b039e64f601aae0d2c55abb3fe41ca4cc4a132ed
Kushagraagarwal <kushagra.agarwal10@gmail.com>
2021-08-22 02:19:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const axios = require('axios').default;
 const app = express()
 const port = process.env.PORT || 8080
-
+const accountSid = 'AC8fd962c55c33e67965461ea8875f2ca3'; 
+const authToken = 'bcc5e866edd8df5a795901efb0b15b9c'; 
+const client = require('twilio')(accountSid, authToken); 
+app.use(express.json())
 
 app.get('/ticker', (req, res) => {
     axios.get('https://api.wazirx.com/api/v2/tickers')
@@ -24,6 +27,17 @@ app.get('/ticker', (req, res) => {
       // handle error
       res.error(error)
     })
+})
+app.post('/sms', (req, res) => {
+    console.log(req.body);
+    client.messages 
+      .create({ 
+         body: 'Your shopsy code is - ' + req.body.code,  
+         messagingServiceSid: 'MG7303e8ec0e4f230feefcf6df16f16924',      
+         to: req.body.number 
+       }) 
+      .then(message => console.log(message.sid)) 
+      .done();
 })
 
 app.listen(port, () => {
